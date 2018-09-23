92f9ef919908c394d819cc3598e71dc79e9fca74
desi <f.desimone30@studenti.unisa.it>
2018-09-23 18:57:24 +0200

    start fixing some problem on reservation

--- contracts/SmartParking.sol
+++ contracts/SmartParking.sol
@@ -2,84 +2,93 @@ pragma solidity ^0.4.24;
 
 import "./Migrations.sol";
 
-contract SmartParking is Migrations{
+contract SmartParking is Migrations { 
 
-    struct Spot{
+    struct Spot {
         uint16 id;
+        uint reservationCount;
+        mapping(uint => Reservation) reservation;
+    }
+
+    struct ParkingArea {
+        uint8 id;
+        uint16 spotCount;
+        int24 lastSpot;
+        uint price;
+        string addrs;
+        mapping(uint16 => Spot) spot;
+    }
+
+    struct Reservation {
         uint start;
         uint creation;
         uint finish;
-        address customer;
         string plate;
         bool paid;
-    }
-
-    struct ParkingArea{
-        uint8 id;
-        uint price;
-        string addrs;
-        uint16 spotCount;
-        int24 lastSpot;
-        mapping(uint16 => Spot) spot;
+        address customer;
     }
 
     function getParkingAreaCount() public view returns(uint){
         return parkingArea.length;
     }
 
-    function isPaid(uint8 idParkingArea, uint16 idSpot) public view returns (bool){
+    function isPaid(uint8 idParkingArea, uint16 idSpot, uint idReservation) public view returns (bool){
         require(idParkingArea<parkingArea.length,"Parking Area out of bound");
         require(idSpot<parkingArea[idParkingArea].spotCount,"Spot out of bound");
-        return parkingArea[idParkingArea].spot[idSpot].paid;
+        return parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].paid;
     }
 
-    function isReservedToMe(uint8 idParkingArea, uint16 idSpot) public view returns (bool){
+    function isReservedToMe(uint8 idParkingArea, uint16 idSpot, uint idReservation) public view returns (bool){
         require(idParkingArea<getParkingAreaCount(),"Parking Area out of bound");
         require(idSpot<parkingArea[idParkingArea].spotCount,"Spot out of bound");
-        return (parkingArea[idParkingArea].spot[idSpot].customer == msg.sender);
+        return (parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].customer == msg.sender);
     }
 
-    function isAvailable(uint8 idParkingArea, uint16 idSpot, uint _now) public view returns (bool){
+    function isAvailable(uint8 idParkingArea, uint16 idSpot, uint idReservation, uint _from, uint _to) public view returns (bool){
         require(idParkingArea<parkingArea.length,"Parking Area out of bound");
         require(idSpot<parkingArea[idParkingArea].spotCount,"Spot out of bound");
-        return (_now > parkingArea[idParkingArea].spot[idSpot].finish ||
-                ((!isPaid(idParkingArea, idSpot)) && (_now > (parkingArea[idParkingArea].spot[idSpot].creation+(15*60))))
+        return (_from > parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].finish ||
+                _to < parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].start ||
+                ((!isPaid(idParkingArea, idSpot, idReservation)) && (_from > (parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].creation+(15*60))))
         );
     }
 
-    function needToPay(uint8 idParkingArea, uint16 idSpot, uint _now) public view returns (bool){
+    function needToPay(uint8 idParkingArea, uint16 idSpot, uint idReservation, uint _now) public view returns (bool){
         require(idParkingArea<getParkingAreaCount(),"Parking Area out of bound");
         require(idSpot<parkingArea[idParkingArea].spotCount,"Spot out of bound");
         return (
-            parkingArea[idParkingArea].spot[idSpot].customer == msg.sender &&
-            !parkingArea[idParkingArea].spot[idSpot].paid &&
-            _now < (parkingArea[idParkingArea].spot[idSpot].creation+(15*60)) &&
-            _now < parkingArea[idParkingArea].spot[idSpot].finish
+            parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].customer == msg.sender && 
+            !parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].paid &&
+            _now < (parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].creation+(15*60)) &&
+            _now < parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].finish
             );
     }
 
-    function getReservation(uint8 idParkingArea, uint16 idSpot,uint _now) public view returns (uint8, uint16, string, uint, uint, uint){
-        require(needToPay(idParkingArea,idSpot,_now),"different customers address in the spot");
+    function getReservation(uint8 idParkingArea, uint16 idSpot, uint idReservation, uint _now) 
+    public view 
+    returns (uint8, uint16, string, uint, uint, uint){
+        require(needToPay(idParkingArea,idSpot,idReservation,_now),"different customers address in the spot");
         return (
                 parkingArea[idParkingArea].id, 
                 parkingArea[idParkingArea].spot[idSpot].id,
                 parkingArea[idParkingArea].addrs,
                 parkingArea[idParkingArea].price,
-                parkingArea[idParkingArea].spot[idSpot].start,
-                parkingArea[idParkingArea].spot[idSpot].finish
+                parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].start,
+                parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].finish
             );
     }
-    function getSpotPaid(uint8 idParkingArea, uint16 idSpot) public view returns (uint8, uint16, string, uint, uint, uint, string){
-        require(isPaid(idParkingArea,idSpot),"This spot is unpaied");
-        require(isReservedToMe(idParkingArea,idSpot),"different customers address in the spot");
+    function getSpotPaid(uint8 idParkingArea, uint16 idSpot,uint idReservation) 
+    public view 
+    returns (uint8, uint16, string, uint, uint, uint){
+        require(isPaid(idParkingArea,idSpot,idReservation),"This spot is unpaied");
+        require(isReservedToMe(idParkingArea,idSpot,idReservation),"different customers address in the spot");
         return(
                 parkingArea[idParkingArea].id, 
                 parkingArea[idParkingArea].spot[idSpot].id,
                 parkingArea[idParkingArea].addrs,
                 parkingArea[idParkingArea].price,
-                parkingArea[idParkingArea].spot[idSpot].start,
-                parkingArea[idParkingArea].spot[idSpot].finish,
-                parkingArea[idParkingArea].spot[idSpot].plate
+                parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].start,
+                parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].finish
             );
     }
 
@@ -99,46 +108,35 @@ contract SmartParking is Migrations{
         parkingArea[id].spotCount = _spotCount;
     }
 
-    function getSpotsAvitable(uint8 idParkingArea, uint _now) public view returns (uint16){
-        require(idParkingArea<parkingArea.length,"Parking Area out of bound");
-        uint16 spotCount = 0;
-        for(uint16 i = 0;i<parkingArea[idParkingArea].spotCount;i++)
-            if(isAvailable(idParkingArea,i,_now)){
-                spotCount++;
-            }
-        return spotCount;
-    }
-
-    function getSpot(uint8 idParkingArea, uint16 idSpot) public view returns (uint16,uint,uint,string){
+    function getSpot(uint8 idParkingArea, uint16 idSpot, uint idReservation) public view returns (uint16,uint,uint,string){
         require(idParkingArea<parkingArea.length,"Parking Area out of bound");
         require(idSpot<parkingArea[idParkingArea].spotCount,"Spot out of bound");
         return (
                 parkingArea[idParkingArea].spot[idSpot].id,
-                parkingArea[idParkingArea].spot[idSpot].start,
-                parkingArea[idParkingArea].spot[idSpot].finish,
-                parkingArea[idParkingArea].spot[idSpot].plate
+                parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].start,
+                parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].finish,
+                parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].plate
             );
     }
 
-    function reserveSpot(uint8 idParkingArea, uint16 idSpot, uint _start, uint _now,uint _finish, string _plate) public{
-        require(isAvailable(idParkingArea,idSpot,_now),"Spot not available");
+    function reserveSpot(uint8 idParkingArea, uint16 idSpot,uint idReservation, uint _start, uint _now, uint _finish, string _plate) public{
+        require(isAvailable(idParkingArea,idSpot,idReservation,_start,_finish),"Spot not available");
         require(_start<_finish,"Start time greater than finish time");
-        parkingArea[idParkingArea].spot[idSpot].start = _start;
-        parkingArea[idParkingArea].spot[idSpot].creation = _now; 
+        parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].start = _start;
+        parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].creation = _now; 
         parkingArea[idParkingArea].spot[idSpot].id = idSpot;
-        parkingArea[idParkingArea].spot[idSpot].finish = _finish; 
-        parkingArea[idParkingArea].spot[idSpot].plate = _plate; 
+        parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].finish = _finish; 
+        parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].plate = _plate; 
         parkingArea[idParkingArea].lastSpot = idSpot;
-        parkingArea[idParkingArea].spot[idSpot].customer = msg.sender;
-        parkingArea[idParkingArea].spot[idSpot].paid = false;
+        parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].customer = msg.sender;
+        parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].paid = false;
     }
 
-    function getParkingArea(uint8 index, uint _now) public view returns (uint8, uint16, uint16, int,uint,string){
-        require(index<parkingArea.length,"Parking Area out of bound");
+    function getParkingArea(uint8 index) public view returns (uint8, uint16, int, uint, string){
+        require(index < parkingArea.length, "Parking Area out of bound");
         return (
                 parkingArea[index].id,
                 parkingArea[index].spotCount,
-                getSpotsAvitable(index,_now),
                 parkingArea[index].lastSpot,
                 parkingArea[index].price,
                 parkingArea[index].addrs
@@ -149,16 +147,18 @@ contract SmartParking is Migrations{
         return owner == msg.sender;
     }
 
-    function getReceipt(uint8 idParkingArea, uint16 idSpot,uint _now) public view returns (uint){
-        require(needToPay(idParkingArea, idSpot,_now),"No receipt found");
-        uint hour = ((((parkingArea[idParkingArea].spot[idSpot].finish-parkingArea[idParkingArea].spot[idSpot].start))/60/60));
+    function getReceipt(uint8 idParkingArea, uint16 idSpot,uint idReservation,uint _now) public view returns (uint){
+        require(needToPay(idParkingArea, idSpot,idReservation,_now),"No receipt found");
+        uint hour = ((((
+            parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].finish-parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].start))/60/60));
         return (hour*(parkingArea[idParkingArea].price));
     }
-    function paySpot(uint8 idParkingArea, uint16 idSpot, uint _now) public payable{
-        require(needToPay(idParkingArea,idSpot,_now),"Spot payment error");
-        uint hour = ((((parkingArea[idParkingArea].spot[idSpot].finish-parkingArea[idParkingArea].spot[idSpot].start))/60/60));
+    function paySpot(uint8 idParkingArea, uint16 idSpot, uint idReservation,uint _now) public payable{
+        require(needToPay(idParkingArea,idSpot,idReservation,_now),"Spot payment error");
+        uint hour = ((((
+            parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].finish-parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].start))/60/60));
         require((hour*(parkingArea[idParkingArea].price)) == msg.value,"The amount send is incorrect");
-        parkingArea[idParkingArea].spot[idSpot].paid = true;
+        parkingArea[idParkingArea].spot[idSpot].reservation[idReservation].paid = true;
         owner.transfer(msg.value);
     }
     
